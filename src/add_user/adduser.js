ba8e1d657d797916e1da0050125f7a8759f06729
Sheamus <c0807605@mylambton.ca>
2022-06-16 18:34:51 -0400

    Add User feature is implemented.

---
+++ src/add_user/adduser.js
@@ -0,0 +1,66 @@
+var form =document.forms["formUser"];
+function handleForm(event) { event.preventDefault(); } 
+form.addEventListener('submit', handleForm);
+
+  function signUp(){
+
+    var strName = form["name"].value;
+    var strSurname = form["surname"].value;
+    var strEmail = form["email"].value;
+    var strPassword = form["password"].value;
+    
+
+    if(isEmpty(strName)){
+        showErrorMessage("Please Enter a Name.");
+        return;
+    }else if(isEmpty(strSurname)){
+        showErrorMessage("Please Enter a Surname.");
+        return;
+    }else if(!isEmailValid(strEmail)){
+        showErrorMessage("Please Enter A Valid Email Address.");
+        return;
+    }else if(isPasswordValid(strPassword) == null){
+        showErrorMessage("Please Enter A Valid Password.");
+        return;
+    }else{
+        firebase.auth().createUserWithEmailAndPassword(strEmail, strPassword).then((success) => {
+            var user = firebase.auth().currentUser;
+            var uid;
+            if (user != null) {
+                uid = user.uid;
+            }
+
+            var userData = {
+                name: strName,
+                surname: strSurname,
+                email: strEmail,
+                password: strPassword
+            }
+
+            db.collection("users")
+            .add(userData)
+            .then((docRef) => 
+            {
+                showSuccessMessage('Your Account Created','Your account was created successfully, you can log in now.',
+                ).then((value) => {
+                    setTimeout(function(){
+                        window.location.replace("../index.html");
+                    }, 1000)
+                });
+            })
+            .catch((error) => {
+                showErrorMessage(error.message);
+            });
+            
+        }).catch((error) => {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            showErrorMessage(errorMessage);
+        });
+    }
+}
+
+  function cancel(){    
+    window.location.href="../main/main.html";
+  }
---
+++ src/common/common.js
@@ -0,0 +1,48 @@
+function isEmpty(string){
+    if(string === ""){
+        return true;
+    }
+    return false;
+}
+// xxxxxxxxxx Email Validation xxxxxxxxxx
+function isEmailValid(userEmail){
+    var userEmailFormate = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(userEmail.match(userEmailFormate)){
+        return true;
+    }else{
+        return false;
+    }
+}
+// xxxxxxxxxx Password Validation xxxxxxxxxx
+function isPasswordValid(userPassword){
+    var userPasswordFormate = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{10,}/;      
+    if(userPassword.match(userPasswordFormate)){
+        return true;
+    }else{
+        return false;
+    }    
+}
+
+function showErrorMessage(message) {
+    return Swal.fire(
+        'Error',
+        message,
+        'error'
+      )
+}
+
+function showInfoMessage(message) {
+    return Swal.fire(
+        'Information',
+        message,
+        'info'
+      )
+}
+
+function showSuccessMessage(message) {
+    return Swal.fire(
+        'Success',
+        message,
+        'success'
+      )
+}
